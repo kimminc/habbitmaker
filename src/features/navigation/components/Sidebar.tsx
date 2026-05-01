0c4c63d72a719965519aab3cd77711a631d0b0dd
kimminc <mingoon78@gmail.com>
2026-05-01 12:36:11 +0900

    fix: import AnimatePresence in Sidebar

--- src/features/navigation/components/Sidebar.tsx
+++ src/features/navigation/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import Image from 'next/image'
 import { LogoutButton } from '@/features/auth/components/LogoutButton'
 import { ThemeToggle } from './ThemeToggle'
